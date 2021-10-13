805f14a6a1c489e3a114b1f6a68ed02b6d68b671
namnguyen <ngnclht@gmail.com>
2021-10-13 23:04:40 +0700

    Init

---
+++ Learn SwiftUI Day 1.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var greeting = "Hello, playground nam"
+var nam = 75
+print(greeting)
+var a = 1
+var b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+
+a = b
+
+print(a)
+a = 22
+print(b)
+
+let constantsIsMe = 3
+// this line should throw error
+//constantsIsMe = 4
+
+// string
+var float:Float = 32.1
+
+var stringsss:String = String(float)
+Int(float)
+let code = """
+    keke;
+    meo meo;
+    """
+print(code.count)
+print(code.uppercased())
+print(code.hasPrefix("keke"))
+
+
+// NUMBER
+let myScore = 12_0
+type(of: myScore)
+
+
