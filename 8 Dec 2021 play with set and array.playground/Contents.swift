31e6ef55742db84c94be054f859cf1fa61124463
Nam Nguyen <ngnclht@gmail.com>
2021-12-08 23:20:34 +0700

    8 Dec 2021

---
+++ 8 Dec 2021 play with set and array.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+var arrayA = [-4, 1, 3, 9 ,4 ,5]
+
+
+var setA = Set(arrayA)
+
+
+var arrayB = [3, 4, 5, 4, 5]
+
+// it automatically remove dupplicated items, cool
+var setB = Set(arrayB)
+
+
+
+print(arrayA.firstIndex(of: 400))
+
+print(arrayA[1])
+print(arrayA[5])
+
+
+print(setA[0])
---
+++ 8 Dec 21 - Play with string.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+
+// variables, simple data types, and string interpolation
+var greeting = "Hello, playground Xin chào các bạn"
+
+var multiline_string = """
+Ok this line
+OKE this line 2
+"""
+
+// capitalized every first character of each word
+print(greeting.capitalized)
+
+
+print(greeting.count)
+
+print("THello \(greeting)")
+
